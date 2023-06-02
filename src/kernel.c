17aeb1ca5ed6672265cdebcec79ea993e33d4d9d
BruceSpruce <bricerandy2001@gmail.com>
2023-06-02 00:41:24 -0500

    entry point

---
+++ src/kernel.c
@@ -0,0 +1,12 @@
+#include "general.h"
+#include "mini_uart.h"
+#include <stdint.h>
+
+void kernel_main(u32 r0, u32 r1, u32 atags) {
+	uart_init();
+	uart_send_string("Hello, Kernel!\n");
+
+	while (1) {
+		uart_send(uart_recv());
+	}
+}

870f68844ff6a8063332e83fd42b910631afcb47
BruceSpruce <bricerandy2001@gmail.com>
2023-06-06 01:53:42 -0500

    correct pi version / -bios?

--- run_qemu.sh
+++ run_qemu.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
-cd ./bin
-qemu-system-arm -M raspi2b -serial stdio -kernel kernel7.elf
+cd bin
+qemu-system-aarch64 -M raspi3b -serial stdio -kernel kernel8.elf
+# note -bios should let us use kernel8.img but not rn?
 cd ..

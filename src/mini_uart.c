35576ed8680aa43c2c5442a032ca6e62f51d88b7
BruceSpruce <bricerandy2001@gmail.com>
2023-06-06 01:52:49 -0500

    general header

--- src/mini_uart.c
+++ src/mini_uart.c
@@ -47,16 +47,16 @@ void uart_init ( void )
 	delay(150);
 	put32(GPIO_PUDCLK0, (1<<14)|(1<<15));
 	delay(150);
-	put32(GPIO_PUDCLK0,0);
+	put32(GPIO_PUDCLK0, 0);
 
-	put32(AUX_ENABLES,1);                   //Enable mini uart (this also enables access to its registers)
-	put32(AUX_MU_CNTL_REG,0);               //Disable auto flow control and disable receiver and transmitter (for now)
-	put32(AUX_MU_IER_REG,0);                //Disable receive and transmit interrupts
-	put32(AUX_MU_LCR_REG,3);                //Enable 8 bit mode
-	put32(AUX_MU_MCR_REG,0);                //Set RTS line to be always high
-	put32(AUX_MU_BAUD_REG,270);             //Set baud rate to 115200
+	put32(AUX_ENABLES,1);                   // Enable mini uart (this also enables access to its registers)
+	put32(AUX_MU_CNTL_REG, 0);               // Disable auto flow control and disable receiver and transmitter (for now)
+	put32(AUX_MU_IER_REG, 0);                // Disable receive and transmit interrupts
+	put32(AUX_MU_LCR_REG, 3);                // Enable 8 bit mode
+	put32(AUX_MU_MCR_REG, 0);                // Set RTS line to be always high
+	put32(AUX_MU_BAUD_REG, 270);             // Set baud rate to 115200
 
-	put32(AUX_MU_CNTL_REG,3);               //Finally, enable transmitter and receiver
+	put32(AUX_MU_CNTL_REG,3);               // Finally, enable transmitter and receiver
 }
 
 static inline void delay(i32 count) {

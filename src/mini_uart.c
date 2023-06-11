0a1c71985acc0397e57313e9375f0b46636a077f
BruceSpruce <bricerandy2001@gmail.com>
2023-06-11 01:11:59 -0500

    align comments

--- src/mini_uart.c
+++ src/mini_uart.c
@@ -35,10 +35,10 @@ void uart_init ( void )
 {
 	u64 selector;
 	selector = get64(GPIO_FSEL1);
-	selector &= ~(7<<12);                   // clean gpio14
-	selector |= 2<<12;                      // set alt5 for gpio14
-	selector &= ~(7<<15);                   // clean gpio15
-	selector |= 2<<15;                      // set alt5 for gpio15
+	selector &= ~(7<<12);                    // clean gpio14
+	selector |= 2<<12;                       // set alt5 for gpio14
+	selector &= ~(7<<15);                    // clean gpio15
+	selector |= 2<<15;                       // set alt5 for gpio15
 	put64(GPIO_FSEL1, selector);
 
 	put64(GPIO_PUD,0);
@@ -47,14 +47,14 @@ void uart_init ( void )
 	delay(150);
 	put64(GPIO_PUDCLK0, 0);
 
-	put64(AUX_ENABLES,1);                   // Enable mini uart (this also enables access to its registers)
+	put64(AUX_ENABLES,1);                    // Enable mini uart (this also enables access to its registers)
 	put64(AUX_MU_CNTL_REG, 0);               // Disable auto flow control and disable receiver and transmitter (for now)
 	put64(AUX_MU_IER_REG, 0);                // Disable receive and transmit interrupts
 	put64(AUX_MU_LCR_REG, 3);                // Enable 8 bit mode
 	put64(AUX_MU_MCR_REG, 0);                // Set RTS line to be always high
 	put64(AUX_MU_BAUD_REG, 270);             // Set baud rate to 115200
 
-	put64(AUX_MU_CNTL_REG,3);               // Finally, enable transmitter and receiver
+	put64(AUX_MU_CNTL_REG,3);                // Finally, enable transmitter and receiver
 }
 
 static inline void delay(i64 count) {
